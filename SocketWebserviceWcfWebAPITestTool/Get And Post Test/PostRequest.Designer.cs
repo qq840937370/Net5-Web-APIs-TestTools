f8be4e661f8779b996c3a8faabe80f741e89aeb4
小白 <2806933146@qq.com>
2021-10-20 08:55:05 +0800

    完成常规Post请求

--- SocketWebserviceWcfWebAPITestTool/Get And Post Test/PostRequest.Designer.cs
+++ SocketWebserviceWcfWebAPITestTool/Get And Post Test/PostRequest.Designer.cs
@@ -29,20 +29,80 @@ namespace WebserviceWcfWebAPITestTool.Get_And_Post_Test
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.txtJSONstr = new System.Windows.Forms.TextBox();
+            this.txtReJson = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Post请求
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL：";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(63, 16);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(806, 27);
+            this.txtURL.TabIndex = 1;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnTest.Location = new System.Drawing.Point(889, 10);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(107, 38);
+            this.btnTest.TabIndex = 2;
+            this.btnTest.Text = "请求";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // txtJSONstr
+            // 
+            this.txtJSONstr.Location = new System.Drawing.Point(12, 65);
+            this.txtJSONstr.Multiline = true;
+            this.txtJSONstr.Name = "txtJSONstr";
+            this.txtJSONstr.Size = new System.Drawing.Size(480, 442);
+            this.txtJSONstr.TabIndex = 4;
+            // 
+            // txtReJson
+            // 
+            this.txtReJson.Location = new System.Drawing.Point(516, 65);
+            this.txtReJson.Multiline = true;
+            this.txtReJson.Name = "txtReJson";
+            this.txtReJson.Size = new System.Drawing.Size(480, 442);
+            this.txtReJson.TabIndex = 5;
+            // 
+            // PostRequest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Post请求";
+            this.ClientSize = new System.Drawing.Size(1008, 519);
+            this.Controls.Add(this.txtReJson);
+            this.Controls.Add(this.txtJSONstr);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.Name = "PostRequest";
             this.Text = "Post请求";
             this.Load += new System.EventHandler(this.Post请求_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox txtJSONstr;
+        private System.Windows.Forms.TextBox txtReJson;
     }
 }
--- SocketWebserviceWcfWebAPITestTool/Get And Post Test/PostRequest.cs
+++ SocketWebserviceWcfWebAPITestTool/Get And Post Test/PostRequest.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,6 +22,61 @@ namespace WebserviceWcfWebAPITestTool.Get_And_Post_Test
         private void Post请求_Load(object sender, EventArgs e)
         {
 
+        }
+
+        // 请求按钮
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string restr;
+
+                string urlstr = txtURL.Text.Trim();
+                string jsonstr = txtJSONstr.Text.Trim();
+                restr=HttpPost(urlstr, jsonstr);
+
+                txtReJson.Text = restr;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Post请求
+        /// </summary>
+        /// <param name="url">URL</param>
+        /// <param name="body">application/json</param>
+        /// <returns></returns>
+        public static string HttpPost(string url, string body)
+        {
+            try
+            {
+                Encoding encoding = Encoding.UTF8;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "POST";
+                request.Accept = "application/json, text/javascript, */*"; //"text/html, application/xhtml+xml, */*";
+                request.ContentType = "application/json; charset=utf-8";
+
+                byte[] buffer = encoding.GetBytes(body);
+                request.ContentLength = buffer.Length;
+                request.GetRequestStream().Write(buffer, 0, buffer.Length);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), encoding))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                var res = (HttpWebResponse)ex.Response;
+                StringBuilder sb = new StringBuilder();
+                StreamReader sr = new StreamReader(res.GetResponseStream(), Encoding.UTF8);
+                sb.Append(sr.ReadToEnd());
+                //string ssb = sb.ToString();
+                throw new Exception(sb.ToString());
+            }
         }
     }
 }
