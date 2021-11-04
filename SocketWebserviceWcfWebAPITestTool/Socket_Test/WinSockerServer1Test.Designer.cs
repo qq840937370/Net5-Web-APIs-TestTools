60a81a5d7ced9cb5daaf1212bc2f3353fbc77605
小白 <2806933146@qq.com>
2021-11-04 09:08:32 +0800

    临时保存

--- SocketWebserviceWcfWebAPITestTool/Socket_Test/WinSockerServer1Test.Designer.cs
+++ SocketWebserviceWcfWebAPITestTool/Socket_Test/WinSockerServer1Test.Designer.cs
@@ -29,12 +29,189 @@ namespace SocketWebserviceWcfWebAPITestTool.Socket_Test
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnBroadcast = new System.Windows.Forms.Button();
+            this.txtIPAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOpenServer = new System.Windows.Forms.Button();
+            this.lblListen = new System.Windows.Forms.Label();
+            this.btnShowCon = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtBvalue = new System.Windows.Forms.TextBox();
+            this.txtAvalue = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBroadcast
+            // 
+            this.btnBroadcast.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnBroadcast.Location = new System.Drawing.Point(314, 61);
+            this.btnBroadcast.Name = "btnBroadcast";
+            this.btnBroadcast.Size = new System.Drawing.Size(129, 56);
+            this.btnBroadcast.TabIndex = 21;
+            this.btnBroadcast.Text = "发送";
+            this.btnBroadcast.UseVisualStyleBackColor = true;
+            this.btnBroadcast.Click += new System.EventHandler(this.btnBroadcast_Click);
+            // 
+            // txtIPAddress
+            // 
+            this.txtIPAddress.Location = new System.Drawing.Point(87, 18);
+            this.txtIPAddress.Name = "txtIPAddress";
+            this.txtIPAddress.Size = new System.Drawing.Size(256, 27);
+            this.txtIPAddress.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "监听地址";
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(349, 17);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(94, 29);
+            this.btnModify.TabIndex = 14;
+            this.btnModify.Text = "修改";
+            this.btnModify.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(24, 70);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(94, 29);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.Text = "关闭监听";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnOpenServer
+            // 
+            this.btnOpenServer.Location = new System.Drawing.Point(24, 13);
+            this.btnOpenServer.Name = "btnOpenServer";
+            this.btnOpenServer.Size = new System.Drawing.Size(94, 29);
+            this.btnOpenServer.TabIndex = 10;
+            this.btnOpenServer.Text = "开启监听";
+            this.btnOpenServer.UseVisualStyleBackColor = true;
+            this.btnOpenServer.Click += new System.EventHandler(this.btnOpenServer_Click);
+            // 
+            // lblListen
+            // 
+            this.lblListen.AutoSize = true;
+            this.lblListen.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblListen.Location = new System.Drawing.Point(628, 26);
+            this.lblListen.Name = "lblListen";
+            this.lblListen.Size = new System.Drawing.Size(159, 19);
+            this.lblListen.TabIndex = 25;
+            this.lblListen.Text = "服务器当前状态：开启";
+            this.lblListen.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // btnShowCon
+            // 
+            this.btnShowCon.Location = new System.Drawing.Point(656, 71);
+            this.btnShowCon.Name = "btnShowCon";
+            this.btnShowCon.Size = new System.Drawing.Size(118, 46);
+            this.btnShowCon.TabIndex = 24;
+            this.btnShowCon.Text = "显示当前连接";
+            this.btnShowCon.UseVisualStyleBackColor = true;
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(12, 147);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(776, 296);
+            this.txtInfo.TabIndex = 23;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Controls.Add(this.btnOpenServer);
+            this.panel1.Location = new System.Drawing.Point(473, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(142, 113);
+            this.panel1.TabIndex = 22;
+            // 
+            // txtBvalue
+            // 
+            this.txtBvalue.Location = new System.Drawing.Point(87, 94);
+            this.txtBvalue.Name = "txtBvalue";
+            this.txtBvalue.Size = new System.Drawing.Size(206, 27);
+            this.txtBvalue.TabIndex = 20;
+            // 
+            // txtAvalue
+            // 
+            this.txtAvalue.Location = new System.Drawing.Point(87, 58);
+            this.txtAvalue.Name = "txtAvalue";
+            this.txtAvalue.Size = new System.Drawing.Size(206, 27);
+            this.txtAvalue.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "b：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "a：";
+            // 
+            // WinSockerServer1Test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBroadcast);
+            this.Controls.Add(this.txtIPAddress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.lblListen);
+            this.Controls.Add(this.btnShowCon);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtBvalue);
+            this.Controls.Add(this.txtAvalue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "WinSockerServer1Test";
             this.Text = "WinSockerServer1Test";
+            this.Load += new System.EventHandler(this.WinSockerServer1Test_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnBroadcast;
+        private System.Windows.Forms.TextBox txtIPAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOpenServer;
+        private System.Windows.Forms.Label lblListen;
+        private System.Windows.Forms.Button btnShowCon;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtBvalue;
+        private System.Windows.Forms.TextBox txtAvalue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
--- SocketWebserviceWcfWebAPITestTool/Socket_Test/WinSockerServer1Test.cs
+++ SocketWebserviceWcfWebAPITestTool/Socket_Test/WinSockerServer1Test.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,206 @@ namespace SocketWebserviceWcfWebAPITestTool.Socket_Test
 {
     public partial class WinSockerServer1Test : Form
     {
+        static ClientWebSocket client = new ClientWebSocket();//实例化客户端对象
+
         public WinSockerServer1Test()
         {
             InitializeComponent();
         }
+
+        private void WinSockerServer1Test_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 打开监听
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOpenServer_Click(object sender, EventArgs e)
+        {
+            if (txtIPAddress.Enabled == true)
+            {
+                MessageBox.Show("请先确认地址");
+                return;
+            }
+
+            string ServerAddress = txtIPAddress.Text;
+            //如果已经连上了服务端，想要再次进行连接，需要进行判断，关闭当前连接后才能进行
+            if (client.State == WebSocketState.Open)
+            {
+                MessageBox.Show("当前client对象连接状态为open，若要重新连接，请先关闭当前连接");
+                return;
+            }
+
+            try
+            {
+                client = new ClientWebSocket();//这一句不要进行状态判断，因为除了Open和Closed，还有Abort等好几种状态。干脆每次连接重新初始化。
+                client.ConnectAsync(new Uri(ServerAddress), CancellationToken.None).Wait();
+                txtInfo.AppendText("开启了连接" + DateTime.Now.ToString() + "\n");
+            }
+            catch (Exception ex)
+            {
+                txtInfo.AppendText(ex.ToString() + DateTime.Now.ToString() + "\n");
+                MessageBox.Show("连接出现问题，请检查网络是否通畅，地址是否正确，服务端是否开启");
+                return;
+            }
+            finally
+            {
+                lblListen.Text = client.State.ToString();
+            }
+
+            StartReceiving(client);
+        }
+
+        #region 方法
+        /// <summary>
+        /// 异步接收服务端数据，获取json数据后反序列化，然后显示到文本框控件中
+        /// </summary>
+        /// <param name="client"></param>
+        private async void StartReceiving(ClientWebSocket client)
+        {
+            if (client.State != WebSocketState.Open)//正常来说进入到此方法的状态都为Open
+            {
+                lblListen.Text = client.State.ToString();
+                MessageBox.Show("StartReceiving方法：连接状态异常,请尝试重新连接");
+                return;
+            }
+            try//有可能中途连接断开
+            {
+                while (true)
+                {
+                    var array = new byte[2048];
+                    if (!((client.State == WebSocketState.Open) || (client.State == WebSocketState.CloseSent)))
+                    {
+                        //接收消息的有效状态是Open和CloseSent，如果不是这两种状态，则退出。
+                        //主动退出也会影响异步线程的接收，因此先进行判断
+                        lblListen.Text = "Closed";
+                        txtInfo.AppendText("StartReceiving方法:连接状态异常，退出循环接收，请检查" + DateTime.Now.ToString() + "\n");
+                        return;
+                    }
+                    var result = await client.ReceiveAsync(new ArraySegment<byte>(array), CancellationToken.None);
+                    if (result.MessageType == WebSocketMessageType.Text)
+                    {
+                        //获取字节数组并转为字符串，此字符串应为json类型，需要反序列化
+                        string jsonmsg = Encoding.UTF8.GetString(array, 0, result.Count);
+
+                        try//将反序列化内容放入try中，避免无法匹配、内容为空等可能报错的地方
+                        {
+                            //将转换后的字符串内容进行json反序列化。参考：https://www.cnblogs.com/yinmu/p/12160343.html
+                            TestValue tv = JsonConvert.DeserializeObject<TestValue>(jsonmsg);
+                            //将收到的a,b的值显示到文本框
+                            if (tv != null)
+                            {
+                                string valueA = string.Empty, valueB = string.Empty;
+                                if (tv.a != null && tv.a.Length > 0) { valueA = tv.a; }
+                                if (tv.a != null && tv.b.Length > 0) { valueB = tv.b; }
+                                txtAvalue.Text = valueA;
+                                txtBvalue.Text = valueB;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            //如果json反序列化出了问题
+                            txtInfo.AppendText(ex.ToString() + DateTime.Now.ToString() + "\n");//将错误类型显示出来
+                            txtInfo.AppendText(jsonmsg + "\n");//将收到的原始字符串显示出来
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)//看看什么类型的错误
+            {
+                lblListen.Text = client.State.ToString();
+                //MessageBox.Show(ex.ToString());//暂且注释，弹出消息框影响观感
+                if (ex.GetType().ToString() == "System.Net.WebSockets.WebSocketException" && client.State != WebSocketState.Open)
+                {
+                    //客户端关闭时会抛出此错误
+                    txtInfo.AppendText("连接被关闭，请检查网络或服务器" + DateTime.Now.ToString() + "\n");
+                }
+                else
+                {
+                    txtInfo.AppendText(ex.ToString() + DateTime.Now.ToString() + "\n");
+                }
+            }
+            //finally
+            //{
+            //    if (client != null)
+            //    {
+            //        client.Dispose();
+            //    }
+            //}
+
+        }
+
+        /// <summary>
+        /// //以dictionary将数据的键值对匹配，然后进行json序列化，避免定义类的麻烦。
+        /// </summary>
+        /// <param name="valueA"></param>
+        /// <param name="valueB"></param>
+        /// <returns></returns>
+        public static string SerializeJson(string valueA, string valueB)
+        {
+            if (valueA.Length == 0) { valueA = "-"; }
+            if (valueB.Length == 0) { valueB = "-"; }
+            //以dictionary将数据的键值对匹配，然后进行json序列化，避免定义类的麻烦。参考：https://www.cnblogs.com/kevinWu7/p/10163455.html
+            Dictionary<string, string> dic = new Dictionary<string, string>(){
+                { "a",valueA },
+                { "b",valueB }
+            };
+            string Jsondata = JsonConvert.SerializeObject(dic);
+            return Jsondata;
+        }
+        #endregion
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBroadcast_Click(object sender, EventArgs e)
+        {
+            if (txtIPAddress.Enabled == true)
+            {
+                MessageBox.Show("请先确认地址");
+                return;
+            }
+            string valueA = string.Empty, valueB = string.Empty;
+            valueA = txtAvalue.Text;
+            valueB = txtBvalue.Text;
+            string jsondata = SerializeJson(valueA, valueB);
+
+            var array = new ArraySegment<byte>(Encoding.UTF8.GetBytes(jsondata));
+            //此处需要捕捉异常，连接是否通畅？
+            try
+            {
+                if (client.State == WebSocketState.Open)//连通状态才允许发送
+                {
+                    client.SendAsync(array, WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+                else
+                {
+                    MessageBox.Show("连接状态异常,请尝试重新连接");
+                }
+            }
+            catch (Exception ex)
+            {
+                txtInfo.AppendText(ex.ToString() + DateTime.Now.ToString() + "\n");
+                return;
+            }
+            finally
+            {
+                lblListen.Text = client.State.ToString();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 用于json反序列化获取实体
+    /// </summary>
+    public class TestValue
+    {
+        public string a { get; set; }
+        public string b { get; set; }
     }
 }
